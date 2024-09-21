ca0ea3b90a5d950778fa07d956b9d5ef2d583ddd
Saint-Pierre Kassi <saint-pierre.kassi@ism.edu.sn>
2024-09-21 16:46:25 +0000

    ajout de la config de mongo okey

---
+++ src/main/java/sn/ksi/gestion_boutique_kassi/config/YamlConfig.java
@@ -0,0 +1,20 @@
+package sn.ksi.gestion_boutique_kassi.config;
+
+import org.springframework.beans.factory.config.YamlPropertiesFactoryBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.io.ClassPathResource;
+
+@Configuration
+public class YamlConfig {
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer properties() {
+        PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer = new PropertySourcesPlaceholderConfigurer();
+        YamlPropertiesFactoryBean yaml = new YamlPropertiesFactoryBean();
+        yaml.setResources(new ClassPathResource("additional-config.yml"));
+        propertySourcesPlaceholderConfigurer.setProperties(yaml.getObject());
+        return propertySourcesPlaceholderConfigurer;
+    }
+}
